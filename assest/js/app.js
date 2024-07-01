59bb1931b53d2200818cee548164e91163ac2c72
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-07-01 16:43:22 +0100

    add

---
+++ assest/js/app.js
@@ -0,0 +1,15 @@
+let currentDay = document.getElementById("current_day");
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let date = new Date();
+
+let day = weekDays[date.getDay()];
+currentDay.innerHTML = day;
+
+let hour = date.getUTCHours();
+let minutes = date.getUTCMinutes();
+let seconds = date.getUTCSeconds();
+
+
+let currentUTC = document.getElementById("current_utc");
+currentUTC.innerHTML = date.getTime();
