e6f1beea37002f2686f9102d3f85452a7353c434
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-07-02 15:15:36 +0100

    codes modified

--- assest/js/app.js
+++ assest/js/app.js
@@ -17,7 +17,8 @@ function updateUTCTime() {
     const now = new Date();
     const hours = String(now.getUTCHours()).padStart(2, '0');
     const minutes = String(now.getUTCMinutes()).padStart(2, '0');
-    const currentTime = `${hours}:${minutes}`;
+    let seconds = String(now.getUTCSeconds()).padStart(2, '0');
+    const currentTime = `${hours}:${minutes}:${seconds}`;
     document.getElementById('current_utc').innerHTML = currentTime+ ' UTC';
 }
 
