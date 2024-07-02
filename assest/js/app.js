de3fda0ffa82b6aedbdef88127b2bc0b155d809f
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-07-02 14:14:06 +0100

    codes modified

--- assest/js/app.js
+++ assest/js/app.js
@@ -6,6 +6,21 @@ let date = new Date();
 let day = weekDays[date.getDay()];
 currentDay.innerHTML = day;
 
+//let hour = date.getUTCHours();
+//let minutes = date.getUTCMinutes();
+//let seconds = date.getUTCSeconds();
+//let currentUTCTime = document.getElementById("current_utc");
+//currentUTCTime.innerHTML = date.getTime();
 
-let currentUTCTime = document.getElementById("current_utc");
-currentUTCTime.innerHTML = date.getTime();
+
+function updateUTCTime() {
+    const now = new Date();
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const currentTime = `${hours}:${minutes}`;
+    document.getElementById('current_utc').innerHTML = currentTime+ ' UTC';
+}
+
+updateUTCTime();
+setInterval(updateUTCTime, 60000);
+
